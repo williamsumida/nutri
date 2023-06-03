f94e106f875b064784144523225c0ad72135d56d
William Sumida <william.h.sumida@gmail.com>
2023-06-03 18:08:13 -0300

    created simple tests for food features

--- backend/src/useCases/GetFood/GetFoodUseCase.spec.ts
+++ backend/src/useCases/GetFood/GetFoodUseCase.spec.ts
@@ -1,3 +1,34 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
+import { InMemoryFoodRepository } from "../../repositories/implementations/InMemoryFoodRepository";
+import { GetFoodUseCase } from "./GetFoodUseCase";
+import { CreateFoodUseCase } from "../CreateFood/CreateFoodUseCase";
+import { Food } from "../../entities/Food";
 
-it("")
+let inMemoryFoodRepository: InMemoryFoodRepository;
+let getFoodUseCase: GetFoodUseCase;
+let createFoodUseCase: CreateFoodUseCase;
+
+describe("Fetching specific food from the database", () => {
+  beforeEach(() => {
+    inMemoryFoodRepository = new InMemoryFoodRepository();
+    getFoodUseCase = new GetFoodUseCase(inMemoryFoodRepository);
+    createFoodUseCase = new CreateFoodUseCase(inMemoryFoodRepository);
+  });
+
+
+  it("should return searched food", async () => {
+    const food_data = {
+      name: "Frango sassami",
+      calories: 93.98,
+      weight: 100,
+      unit: "grams",
+    };
+
+    const food = await createFoodUseCase.execute(food_data);
+    const { id } = food;
+
+    const fetched_food = await getFoodUseCase.execute({ id });
+
+    expect(fetched_food).toEqual(food);
+  });
+})
--- backend/src/useCases/GetFoods/GetFoodsUseCase.spec.ts
+++ backend/src/useCases/GetFoods/GetFoodsUseCase.spec.ts
@@ -1,3 +1,45 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
+import { InMemoryFoodRepository } from "../../repositories/implementations/InMemoryFoodRepository";
+import { GetFoodsUseCase } from "./GetFoodsUseCase";
+import { CreateFoodUseCase } from "../CreateFood/CreateFoodUseCase";
+import { Food } from "../../entities/Food";
 
-it("")
+let inMemoryFoodRepository: InMemoryFoodRepository;
+let getFoodsUseCase: GetFoodsUseCase;
+let createFoodUseCase: CreateFoodUseCase;
+
+describe("Fetching all foods from the database", () => {
+  beforeEach(() => {
+    inMemoryFoodRepository = new InMemoryFoodRepository();
+    getFoodsUseCase = new GetFoodsUseCase(inMemoryFoodRepository);
+    createFoodUseCase = new CreateFoodUseCase(inMemoryFoodRepository);
+  });
+
+  it("should return an empty list", async () => {
+    const foods = await getFoodsUseCase.execute();
+    expect(foods).toEqual([]);
+  });
+
+  it("should return all inserted foods", async () => {
+    const food_data1 = {
+      name: "Frango sassami",
+      calories: 93.98,
+      weight: 100,
+      unit: "grams",
+    };
+
+    const food_data2 = {
+      name: "Frango sassami",
+      calories: 93.98,
+      weight: 100,
+      unit: "grams",
+    };
+
+    const food1 = await createFoodUseCase.execute(food_data1);
+    const food2 = await createFoodUseCase.execute(food_data2);
+
+    const foods = await getFoodsUseCase.execute();
+
+    expect(foods).toEqual([food1, food2]);
+  });
+})
