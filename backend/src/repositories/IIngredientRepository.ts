c35181646c7270c85e44a5237ab32e9d5c1a22a9
William Sumida <william.h.sumida@gmail.com>
2023-06-15 00:19:27 -0300

    chore: fetching ingredient and food from database

--- backend/src/repositories/IIngredientRepository.ts
+++ backend/src/repositories/IIngredientRepository.ts
@@ -2,5 +2,6 @@ import { Ingredient } from "../entities/Ingredient";
 
 export interface IIngredientRepository {
   save(ingredient: Ingredient): Promise<Ingredient>;
+  findById(id: string): Promise<Ingredient>;
   findByName(name: string): Promise<Ingredient>;
 }
--- backend/src/repositories/implementations/PostgresIngredientRepository.ts
+++ backend/src/repositories/implementations/PostgresIngredientRepository.ts
@@ -1,5 +1,6 @@
 import { IIngredientRepository } from "../IIngredientRepository";
 import { Ingredient } from "../../entities/Ingredient";
+import { Food } from "../../entities/Food";
 import logger from "../../logger";
 
 import { db } from "../../database/postgres";
@@ -14,6 +15,28 @@ export class PostgresIngredientRepository implements IIngredientRepository {
 
     const { id, name, amount, amountUnit, calories } = result[0];
     return new Ingredient(name, amount, amountUnit, ingredient.food, calories, id);
+  }
+
+  async findById(id: string): Promise<Ingredient> {
+    const result = await db`
+      SELECT *
+      FROM ingredient
+      WHERE id = ${id};
+    `;
+    const { name, amount, amountUnit, calories, food_id } = result[0];
+
+    const foodResult = db`SELECT * FROM food WHERE id = ${food_id}`;
+
+    const food = new Food(
+      foodResult[0].name,
+      foodResult[0].calories,
+      foodResult[0].weight,
+      foodResult[0].unit,
+      foodResult[0].calories_per_unit,
+      foodResult[0].id,
+
+    );
+    return new Ingredient(name, amount, amountUnit, food, calories, id);
   }
 
   async findByName(name: string): Promise<Ingredient> {
